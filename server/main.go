df47309398a71c0a16c3b7e1abce028b9d4e75c0
Mr BeetMaker <pwollsen@gmail.com>
2026-09-07 19:25:04 +0200

    Added connection to optimizer service

--- server/main.go
+++ server/main.go
@@ -4,9 +4,10 @@ import (
 	"log"
 	"net"
 
-	fleetpb "github.com/MrBeetMaker/fleet-optimization/proto"
-
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	fleetpb "github.com/MrBeetMaker/fleet-optimization/proto"
 )
 
 func main() {
@@ -16,11 +17,31 @@ func main() {
 		log.Fatal(err)
 	}
 
+	optimizerConn, err := grpc.NewClient(
+		"localhost:50052",
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer optimizerConn.Close()
+
+	optimizerClient := fleetpb.NewOptimizerServiceClient(optimizerConn)
+
 	grpcServer := grpc.NewServer()
 
-	fleetpb.RegisterFleetServiceServer(grpcServer, NewFleetServer())
+	fleetServer := NewFleetServer(optimizerClient)
+
+	fleetpb.RegisterFleetServiceServer(
+		grpcServer,
+		fleetServer,
+	)
+
+	go fleetServer.RunPeriodicRoutePlanning()
 
 	log.Println("Server listening on :50051")
 
-	grpcServer.Serve(lis)
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatal(err)
+	}
 }
