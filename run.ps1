fb9a55ac4d0141795189fa3937cd3957d653f836
Mr BeetMaker <pwollsen@gmail.com>
2026-09-10 20:07:45 +0200

    Added database setup

--- run.ps1
+++ run.ps1
@@ -1,17 +1,24 @@
+# Environment
+$env:PYTHONPATH = "$PSScriptRoot;${PSScriptRoot}\proto"
+$python = "$PSScriptRoot\.venv\Scripts\python.exe"
+
 function Wait-ForPort($port) {
     while (-not (Test-NetConnection localhost -Port $port -InformationLevel Quiet)) {
         Start-Sleep -Milliseconds 500
     }
 }
 
+# Database
+.\database\setup.ps1
+
 # Optimizer
-Start-Process powershell -ArgumentList "-NoExit", "-Command", "`$env:PYTHONPATH='.;proto'; .\.venv\Scripts\python.exe -m optimizer.main"
+Start-Process powershell -ArgumentList "-NoExit", "-Command", "& '$python' -m optimizer.main"
 Wait-ForPort 50052
 
 # Server
-Start-Process powershell -ArgumentList "-NoExit", "-Command", "go run .\server"
+Start-Process powershell -ArgumentList "-NoExit", "-Command", "Set-Location '$PSScriptRoot'; go run .\server"
 Wait-ForPort 50051
 
 # Client
-Start-Process powershell -ArgumentList "-NoExit", "-Command", "go run .\client"
+Start-Process powershell -ArgumentList "-NoExit", "-Command", "Set-Location '$PSScriptRoot'; go run .\client"
 
